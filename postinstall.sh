45b43eb6e46e95607e2a36d717641f9413dc7e53
David Fridley <ddfridley@yahoo.com>
2017-08-21 22:07:25 -0700

    favicon added to assets

--- postinstall.sh
+++ postinstall.sh
@@ -5,5 +5,6 @@ babel --out-dir dist src &&
 browserify dist/*.js -o dist/demo/demo.js && 
 cp src/demo.html dist/demo && 
 cp src/demo.css dist/demo &&
-cp -r assets/images dist/demo
+cp -r assets/images dist/demo && 
+cp -r assets/favicon dist/demo/favicon
 
