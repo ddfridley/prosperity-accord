f9d9ebca2cfa3277268ea254a71a6c3ed3708d4a
ddfridley <ddfridley@yahoo.com>
2018-08-28 21:48:55 -0700

    config in dist and demo

--- postinstall.sh
+++ postinstall.sh
@@ -2,11 +2,13 @@
 
 mkdir -p dist/demo
 babel --out-dir dist src &&
+echo "window.mailChimpUser=$MAIL_CHIMP_USER; window.mailChimpId=$MAIL_CHIMP_ID" > dist/config.json && 
+echo "window.mailChimpUser=$MAIL_CHIMP_USER; window.mailChimpId=$MAIL_CHIMP_ID" > dist/demo/config.json && 
 browserify dist/*.js -o dist/demo/demo.js && 
 cp src/demo.html dist/demo && 
 cp src/demo.css dist/demo &&
 cp node_modules/react-table/react-table.css dist/demo &&
 cp -r assets/images dist/demo && 
 cp -r assets/favicon dist/demo/favicon
-echo "window.mailChimpUser=$MAIL_CHIMP_USER; window.mailChimpId=$MAIL_CHIMP_ID" > dist/demo/config.json
+
 
