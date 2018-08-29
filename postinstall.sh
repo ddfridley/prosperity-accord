95bf3e3a94b00eb5f83312ec5eea3dc575f0c82e
ddfridley <ddfridley@yahoo.com>
2018-08-28 21:57:45 -0700

    config.json as json

--- postinstall.sh
+++ postinstall.sh
@@ -2,7 +2,7 @@
 
 mkdir -p dist/demo
 babel --out-dir dist src &&
-echo "{mailChimpUser: '$MAIL_CHIMP_USER', mailChimpId='$MAIL_CHIMP_ID'}" > dist/config.json && 
+echo "{mailChimpUser: '$MAIL_CHIMP_USER', mailChimpId: '$MAIL_CHIMP_ID'}" > dist/config.json && 
 browserify dist/*.js -o dist/demo/demo.js && 
 cp src/demo.html dist/demo && 
 cp src/demo.css dist/demo &&
